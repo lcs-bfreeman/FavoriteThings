d43e691e1d0aeebcd1e7b451f640ad7817a317ec
Benjamn <bfreeman@lcs.on.ca>
2021-02-04 22:46:02 -0500

    Just brakmen

--- FavoriteThings/ContentView.swift
+++ FavoriteThings/ContentView.swift
@@ -18,7 +18,10 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+            ContentView()
+        }
     }
 }
 
